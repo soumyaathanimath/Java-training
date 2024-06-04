319643d54cc76111628e8c1683eb14e7ba49ac89
soumyaathanimath <168898037+soumyaathanimath@users.noreply.github.com>
2024-06-04 18:01:45 +0530

    practise programs

---
+++ palindrome/Test.java
@@ -0,0 +1,22 @@
+package palindrome;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   //Scanner sc=new Scanner(System.in);
+		                                            //Input: str = “abba”  Output: Yes
+				                                    //Input: str = “geeks” Output: No
+	   //System.out.println("enter the string");
+	   //String input = sc.next();
+	   
+		String str1="geeks";
+	   for(int i=0;i<str1.length();i++)
+			
+	System.out.println(str1.charAt(i));
+        
+	}
+
+}
